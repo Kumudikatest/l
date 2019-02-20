e0afdcdef6e1f727a5561d66d9c0a4d0880ff7bd
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-02-20 10:49:04 +0530

    tt

--- l/lambda.js
+++ l/lambda.js
@@ -1,4 +1,9 @@
 exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+
+event.Records.forEach((record) => {
+        console.log(record.eventID);
+        console.log(record.eventName);
+        console.log('DynamoDB Record: %j', record.dynamodb);
+    });
+    return `Successfully processed ${event.Records.length} records.`;
 }
