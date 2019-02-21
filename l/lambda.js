cd915c6f8a8a78eb32f892e57d7acef5fb977cd5
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-02-21 14:11:52 +0530

    test

--- l/lambda.js
+++ l/lambda.js
@@ -1,6 +1,6 @@
 let udith_layer = require('/opt/sample-lambda-layer');
 
 exports.handler = function(event, context, callback) {
-    console.log(udith_layer);
+    
     callback(null, {"message": "Successfully executed"});
 }
--- l/public.js
+++ l/public.js
@@ -1,4 +1,3 @@
-let bash = require('/opt/git-lambda-layer');
 exports.handler = function(event, context, callback) {
     
     callback(null, {"message": "Successfully executed"});
