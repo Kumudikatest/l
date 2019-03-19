41020b674b0f47dbdd0d5659cce418e847590c3d
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-03-19 12:56:09 +0530

    test

--- l/lambda.js
+++ l/lambda.js
@@ -1,23 +1,4 @@
-const mysql = require('mysql');
-
 exports.handler = function(event, context, callback) {
-   
-    var connection = mysql.createConnection({
-        host: process.env.host,
-        user: process.env.user,
-        port: process.env.port,
-        database: process.env.database
-    });
-    
-    connection.connect();
-
-    connection.query('SELECT * FROM family LIMIT 5', function (error, results, fields) {
-        if (error) throw error;
-        console.log('The solution is: ', results);
-        callback(null, { results });
-    });
-
-    connection.end();
 
     callback(null, {"message": "Successfully executed"});
 }
