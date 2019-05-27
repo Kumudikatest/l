1e05d8d2d14dc48165a288c82f2d4ceae22064e1
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-05-27 14:24:10 +0530

    remove

--- l/lambda.js
+++ l/lambda.js
@@ -1,17 +1,5 @@
-let SL_TWITTER = require('slappforge-sdk-twitter');
-let twitterClients = require('./TwitterClients');
-const twitter = new SL_TWITTER.TwitterP(twitterClients);
-
 exports.handler = function (event, context, callback) {
-    twitter.getTweet({
-        "tweetID": "Test",
-        "clientName": "twClient"
-    }).then(response => {
-        let data = response.data;
-    }).catch(err => {
-        console.log(err);
-    });
-
+   
 
     callback(null, { "message": "Successfully executed" });
 }
