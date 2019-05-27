19aa6e22153000416c1c0754067b70525c64fa1e
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-05-27 13:57:12 +0530

    add twitter

--- l/lambda.js
+++ l/lambda.js
@@ -1,4 +1,16 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let SL_TWITTER = require('slappforge-sdk-twitter');
+let twitterClients = require('./TwitterClients');
+const twitter = new SL_TWITTER.TwitterP(twitterClients);
+
+exports.handler = function (event, context, callback) {
+    twitter.postTweet({
+        "status": "Test",
+        "clientName": "twClient"
+    }).then(response => {
+        let data = response.data;
+    }).catch(err => {
+        console.log(err);
+    });
+
+    callback(null, { "message": "Successfully executed" });
 }
