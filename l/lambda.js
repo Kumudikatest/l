ce8bfe429bd51437cd2acd0111128091cb1d31a0
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-05-27 16:51:54 +0530

    add cog

--- l/lambda.js
+++ l/lambda.js
@@ -1,3 +1,5 @@
+let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 let SL_TWITTER = require('slappforge-sdk-twitter');
 let twitterClients = require('./TwitterClients');
 const twitter = new SL_TWITTER.TwitterP(twitterClients);
@@ -11,6 +13,16 @@ exports.handler = function (event, context, callback) {
         let data = response.data;
     }).catch(err => {
         console.log(err);
+        cognito_idp.listUsers({
+            UserPoolId: "us-east-1_D10y3fy0o",
+            Limit: "10"
+        }, function (error, data) {
+            if (error) {
+                // implement error handling logic here
+                throw error;
+            }
+            // your logic goes within this block
+        });
     });
 
     callback(null, { "message": "Successfully executed" });
