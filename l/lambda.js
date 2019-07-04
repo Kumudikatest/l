7705e969194940ef5362501c3c63a222c6d582d1
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-07-04 15:59:42 +0530

    test

--- l/lambda.js
+++ l/lambda.js
@@ -7,21 +7,16 @@ exports.handler = function (event, context, callback) {
     // You can pass the existing connection to this function.
     // A new connection will be created if it's not present as the third param 
     // You must always end/destroy the DB connection after it's used
-    rds.query({
-        instanceIdentifier: 'k',
-        query: 'INSERT INTO Test (ID);',
-        inserts: [1]
-    }, function (error, results, connection) {
-        if (error) {
-            console.log("Error occurred");
-            throw error;
-        } else {
-            console.log("Success")
-            console.log(results);
-        }
+                // You must always end/destroy the DB connection after it's used
+                rds.beginTransaction({
+                    instanceIdentifier: 'k'
+                },function (error,connection) {
+                    if (error) {
+                        throw error;
+                    }
 
-        connection.end();
-    });
+                    connection.end();
+                });
 
     callback(null, { "message": "Successfully executed" });
 }
