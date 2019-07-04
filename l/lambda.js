9e8ec0aa947db6d5cb545d2c65eedd791c8390fa
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-07-04 15:44:01 +0530

    test

--- l/lambda.js
+++ l/lambda.js
@@ -1,16 +1,26 @@
-let AWS = require('aws-sdk');
-const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+let SL_AWS = require('slappforge-sdk-aws');
+let connectionManager = require('./ConnectionManager');
+const rds = new SL_AWS.RDS(connectionManager);
 
 exports.handler = function (event, context, callback) {
-    cognito_idp.listUsers({
-        UserPoolId: "us-east-1_D10y3fy0o",
-        Limit: 10
-    }, function (error, data) {
+
+    // You can pass the existing connection to this function.
+    // A new connection will be created if it's not present as the third param 
+    // You must always end/destroy the DB connection after it's used
+    rds.query({
+        instanceIdentifier: 'k',
+        query: 'INSERT INTO Test (ID);',
+        inserts: [1]
+    }, function (error, results, connection) {
         if (error) {
-            // implement error handling logic here
+            console.log("Error occurred");
             throw error;
+        } else {
+            console.log("Success")
+            console.log(results);
         }
-        // your logic goes within this block
+
+        connection.end();
     });
 
     callback(null, { "message": "Successfully executed" });
