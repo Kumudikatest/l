023948392b956fdec3583cd06075f2010ba4c14a
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-07-05 13:37:27 +0530

    test

--- l/lambda.js
+++ l/lambda.js
@@ -1,4 +1,20 @@
+let AWS = require('aws-sdk');
+let SL_AWS = require('slappforge-sdk-aws');
+const sqs = new SL_AWS.SQS(AWS);
+
 exports.handler = function (event, context, callback) {
+    sqs.sendMessage({
+        MessageBody: 'k',
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/KTestSQS`,
+        DelaySeconds: '0',
+        MessageAttributes: {}
+    }, function (data) {
+        console.log(data);
+        // your logic (logging etc) to handle successful message delivery, should be here
+    }, function (error) {
+        // your logic (logging etc) to handle failures, should be here
+    });
+
 
     callback(null, { "message": "Successfully executed" });
 }
