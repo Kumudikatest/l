4c7f65dbb2b60b1e07e38faa2360a6ed941b1f56
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-07-05 14:01:20 +0530

    test

--- l/lambda.js
+++ l/lambda.js
@@ -4,30 +4,17 @@ const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
 
-    sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Newk`,
-        AttributeNames: ['SenderId'],
-        MaxNumberOfMessages: '1',
-        VisibilityTimeout: '30',
-        WaitTimeSeconds: '0',
-        MessageAttributeNames: ['newtest']
-    }).promise()
-        .then(receivedMsgData => {
-            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
-                let receivedMessages = receivedMsgData.Messages;
-                receivedMessages.forEach(message => {
-                    // your logic to access each message through out the loop. Each message is available under variable message 
-                    // within this block
-                    console.log(receivedMessages);
-                });
-            } else {
-                // No messages to process
-            }
-        })
-        .catch(err => {
-            // error handling goes here
-            console.log(err);
-        });
+    sqs.sendMessage({
+        MessageBody: 'k1',
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/k`,
+        DelaySeconds: '0',
+        MessageAttributes: {}
+    }, function (data) {
+        console.log(data);
+        // your logic (logging etc) to handle successful message delivery, should be here
+    }, function (error) {
+        // your logic (logging etc) to handle failures, should be here
+    });
 
     callback(null, { "message": "Successfully executed" });
 }
