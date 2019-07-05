c685b044b564085ab46ddafa471e7d839bf64860
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-07-05 13:44:49 +0530

    test

--- l/lambda.js
+++ l/lambda.js
@@ -3,18 +3,30 @@ let SL_AWS = require('slappforge-sdk-aws');
 const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
-    sqs.sendMessage({
-        MessageBody: 'k',
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/KTestSQS`,
-        DelaySeconds: '0',
-        MessageAttributes: {}
-    }, function (data) {
-        console.log(data);
-        // your logic (logging etc) to handle successful message delivery, should be here
-    }, function (error) {
-        // your logic (logging etc) to handle failures, should be here
-    });
 
+    sqs.receiveMessage({
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Newk`,
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '1',
+        VisibilityTimeout: '30',
+        WaitTimeSeconds: '0'
+    }).promise()
+        .then(receivedMsgData => {
+            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
+                let receivedMessages = receivedMsgData.Messages;
+                receivedMessages.forEach(message => {
+                    // your logic to access each message through out the loop. Each message is available under variable message 
+                    // within this block
+                    console.log(receivedMessages);
+                });
+            } else {
+                // No messages to process
+            }
+        })
+        .catch(err => {
+            // error handling goes here
+            console.log(err);
+        });
 
     callback(null, { "message": "Successfully executed" });
 }
